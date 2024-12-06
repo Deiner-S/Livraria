28f68dd75cb9bd9110b3cc48446ab6da600a6978
Deiner Rodrigues de Souza <deiner.souza@win.ufu.br>
2024-12-06 17:47:59 -0300

    implementando front-end sem javascript junto aos endpoints do back-end

--- src/app2.py
+++ src/app2.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         #processando os dados do formulário
@@ -11,7 +15,20 @@ def login():
         print(f"Login efetuado com usuário {username}") 
 
     
-    return render_template('index.html')
+    return render_template('login.html')
+
+
+
+@app.route('/cadastro', methods=['GET','POST'])
+def cadastro_cliente():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        cpf = request.form.get("cpf")
+        cnpj = request.form.get("cnpj")
+        ie = request.form.get("ie")
+        print(f"nome: {nome},\n cpf: {cpf}, \n cnpj: {cnpj}, \n ie: {ie}")
+    
+    return render_template("cadastro_cliente.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
